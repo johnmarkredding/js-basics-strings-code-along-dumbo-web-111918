91f84b5db48d24f04f01fbb890576ec66464499f
John Mark Redding <johnmarkredding@gmail.com>
2019-01-05 02:18:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 currentUser = "John Mark";
 welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.uppercase}!`;
